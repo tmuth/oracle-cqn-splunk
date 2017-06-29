6dd45fafee146f99209e822e460684f537e5fbb2
Tyler D Muth <tmuth@splunk.com>
2017-06-29 13:55:02 -0400

    Added logger to install to allow debugging.

---
+++ src/install.sql
@@ -0,0 +1,10 @@
+@../logger/logger_install.sql
+-- Use the following instead if you don't want to install logger:
+-- @../logger/logger_no_op.sql
+
+@cqn-tables.sql
+
+@splunk_util.pks
+@splunk_util.pkb
+
+@cqn-register.sql
